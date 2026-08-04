78183504df4a765c0e95069c4fd1f111d3239f89
Bhanu Pandey <bhanupandey0843@gmail.com>
2026-08-04 17:05:01 +0530

    added the home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import { AboutShowcase } from "@/components/sections/about-showcase";
+import { ContactCta } from "@/components/sections/contact-cta";
 import { Hero } from "@/components/sections/hero";
+import { Services } from "@/components/sections/services";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Services />
+      <AboutShowcase />
+      <ContactCta />
+    </>
+  );
 }
